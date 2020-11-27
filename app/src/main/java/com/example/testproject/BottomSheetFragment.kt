ed21d5e863b205f873b9cefeb40481f6563e7d6f
afridi <afridi.mahafuz@gmail.com>
2020-11-27 14:05:29 +0600

    normal and large screen designed.

--- app/src/main/java/com/example/testproject/BottomSheetFragment.kt
+++ app/src/main/java/com/example/testproject/BottomSheetFragment.kt
@@ -4,20 +4,159 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.testproject.custom.SizeAdapter
+import com.example.testproject.pojoClasses.Size
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import kotlinx.android.synthetic.main.bottom_sheet_fragment.*
+import kotlinx.android.synthetic.main.bottom_sheet_fragment.view.*
 
 class BottomSheetFragment : BottomSheetDialogFragment() {
+
+    lateinit var sizeAdapter: SizeAdapter
+    lateinit var list: MutableList<Size>
+    var itemQuantity: Int = 0
+    private val images = intArrayOf(
+        R.drawable.cover_image,
+        R.drawable.image2,
+        R.drawable.cover_image,
+        R.drawable.image2,
+        R.drawable.cover_image
+    )
+    private var imageIndex: Int = 0
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.bottom_sheet_fragment,container,false)
+        return inflater.inflate(R.layout.bottom_sheet_fragment, container, false)
     }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        list = ArrayList()
+        sizeAdapter = SizeAdapter(context!!, list)
+
+        view.sizeRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+            adapter = sizeAdapter
+        }
+
+        addItem.setOnClickListener {
+            itemQuantity++
+            tv_quantity.text = itemQuantity.toString()
+        }
+
+        removeItem.setOnClickListener {
+            if (itemQuantity > 0) {
+                itemQuantity--
+                tv_quantity.text = itemQuantity.toString()
+            }
+        }
+
+        tv_quantity.text = itemQuantity.toString()
+
+        addSizesToSizeList()
+
+        //imageSwitcher
+        imageSwitcher?.setFactory {
+            val imgView = ImageView(context)
+            imgView.scaleType = ImageView.ScaleType.FIT_XY
+            imgView
+        }
+
+        imageSwitcher?.setImageResource(images[imageIndex])
+        val imgIn = AnimationUtils.loadAnimation(
+            context, android.R.anim.slide_in_left
+        )
+
+        imageSwitcher?.inAnimation = imgIn
+
+        val imgOut = AnimationUtils.loadAnimation(
+            context, android.R.anim.slide_out_right
+        )
+
+        imageSwitcher?.outAnimation = imgOut
+
+        prev_imsg_layout.setOnClickListener {
+            if (imageIndex - 1 >= 0) {
+                imageIndex -= 1
+                imageSwitcher?.setImageResource(images[imageIndex])
+                if(imageIndex==0){
+                    prev_imsg_layout.visibility = View.INVISIBLE
+                    next_img_layout.visibility = View.VISIBLE
+                }
+                else{
+                    prev_imsg_layout.visibility = View.VISIBLE
+                    next_img_layout.visibility = View.VISIBLE
+                }
+            }
+        }
+
+        next_img_layout.setOnClickListener {
+            if (imageIndex + 1 < images.size) {
+                imageIndex += 1
+                imageSwitcher?.setImageResource(images[imageIndex])
+                if(imageIndex == images.size - 1){
+                    next_img_layout.visibility = View.INVISIBLE
+                    prev_imsg_layout.visibility = View.VISIBLE
+                }else{
+                    next_img_layout.visibility = View.VISIBLE
+                    prev_imsg_layout.visibility = View.VISIBLE
+                }
+            }
+        }
+
+        if(imageIndex==0){
+            prev_imsg_layout.visibility = View.INVISIBLE
+        }
+
+        img1Layout.setOnClickListener {
+            imageSwitcher?.setImageResource(images[0])
+            imageIndex=0
+            prev_imsg_layout.visibility = View.INVISIBLE
+            next_img_layout.visibility = View.VISIBLE
+        }
+        img2Layout.setOnClickListener {
+            imageIndex=1
+            imageSwitcher?.setImageResource(images[1])
+            next_img_layout.visibility = View.VISIBLE
+            prev_imsg_layout.visibility = View.VISIBLE
+        }
+        img3Layout.setOnClickListener {
+            imageIndex=2
+            imageSwitcher?.setImageResource(images[2])
+            next_img_layout.visibility = View.VISIBLE
+            prev_imsg_layout.visibility = View.VISIBLE
+        }
+        img4Layout.setOnClickListener {
+            imageIndex=3
+            imageSwitcher?.setImageResource(images[3])
+            next_img_layout.visibility = View.VISIBLE
+            prev_imsg_layout.visibility = View.VISIBLE
+        }
+        img5Layout.setOnClickListener {
+            imageIndex=4
+            imageSwitcher?.setImageResource(images[4])
+            next_img_layout.visibility = View.INVISIBLE
+            prev_imsg_layout.visibility = View.VISIBLE
+        }
+
+
+
+    }
+
+    private fun addSizesToSizeList() {
+        list.add(Size("S"))
+        list.add(Size("M"))
+        list.add(Size("X"))
+        list.add(Size("XL"))
+
+        sizeAdapter.notifyDataSetChanged()
     }
 }
---
+++ app/src/main/java/com/example/testproject/custom/SizeAdapter.kt
@@ -0,0 +1,49 @@
+package com.example.testproject.custom
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.example.testproject.R
+import com.example.testproject.pojoClasses.Size
+import kotlinx.android.synthetic.main.show_size_demo.view.*
+
+class SizeAdapter(val context: Context, val sizeList: MutableList<Size>) :
+    RecyclerView.Adapter<SizeAdapter.SizeViewHolder>() {
+
+    var selectedItem : Int = -1
+
+    inner class SizeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SizeViewHolder {
+
+        return SizeViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.show_size_demo,parent,false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return sizeList.size
+    }
+
+    override fun onBindViewHolder(holder: SizeViewHolder, position: Int) {
+
+        holder.itemView.tv_size.text = sizeList[position].sizeName
+        holder.itemView.sizeLayout.setOnClickListener {
+            selectedItem = position
+            notifyDataSetChanged()
+            Toast.makeText(context, sizeList[position].sizeName, Toast.LENGTH_SHORT).show()
+        }
+
+        if(selectedItem == position){
+            holder.itemView.sizeLayout.setBackgroundResource(R.drawable.size_layout_bg_after_press)
+        }
+        else{
+            holder.itemView.sizeLayout.setBackgroundResource(R.drawable.size_layout_bg)
+        }
+    }
+
+
+}
